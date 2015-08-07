0df1f48d7bcc572b03fe3c4b58c280e27675e2ce
KuoE0 <kuoe0.tw@gmail.com>
2015-08-07 14:56:28 +0800

    get request token successfully

--- js/app.js
+++ js/app.js
@@ -1,29 +1,71 @@
+
+'use strict';
+
+function debug(str) {
+	console.log("-*- FoxPocket -*- " + str);
+}
+
+var Pocket = {
+
+	REDIRECT_URI: "app://772bb765-bb36-4242-b179-55688b5df274/manifest.webapp",
+
+	get CONSUMER_KEY() {
+		if (this._CONSUMER_KEY === undefined) {
+			this._CONSUMER_KEY = gCONSUMER_KEY;
+		}
+		debug("CONSUMER_KEY: " + this._CONSUMER_KEY);
+		return this._CONSUMER_KEY;
+	},
+
+	authenticate: function(callback) {
+		debug("start authenticating...");
+		this._post(
+			"https://getpocket.com/v3/oauth/request",
+			JSON.stringify({
+				consumer_key: this.CONSUMER_KEY,
+				redirect_uri: this.REDIRECT_URI
+			}),
+			response => {
+				this._storeAccessToken(response.code, callback);
+			}
+		);
+	},
+
+	_post: function(url, data, callback) {
+		debug("_post");
+		var request = new XMLHttpRequest({
+			mozSystem: true
+		});
+
+		debug(data);
+		request.addEventListener("load", evt => {
+
+			debug("request status: " + request.status);
+			if (request.status === 200 && callback) {
+				var response = JSON.parse(request.responseText);
+				callback(response);
+			}
+
+		}, false);
+
+		request.open("POST", url);
+		request.setRequestHeader("Content-type", "application/json; charset=UTF8");
+		request.setRequestHeader("X-Accept", "application/json");
+		request.send(data || null);
+	},
+
+	_storeAccessToken: function(accessToken, callback) {
+		debug("get access token...");
+		debug("access token: " + accessToken);
+	}
+
+};
+
 // DOMContentLoaded is fired once the document has been loaded and parsed,
 // but without waiting for other external resources to load (css/images/etc)
 // That makes the app more responsive and perceived as faster.
 // https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
 window.addEventListener('DOMContentLoaded', function() {
-
-  // We'll ask the browser to use strict code to help us catch errors earlier.
-  // https://developer.mozilla.org/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
-  'use strict';
-
-  var translate = navigator.mozL10n.get;
-
-  // We want to wait until the localisations library has loaded all the strings.
-  // So we'll tell it to let us know once it's ready.
-  navigator.mozL10n.once(start);
-
-  // ---
-
-  function start() {
-
-    var message = document.getElementById('message');
-
-    // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
-    // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
-    message.textContent = translate('message');
-
-  }
-
+	document.getElementById('btn-auth').addEventListener('click', Pocket.authenticate.bind(Pocket));
 });
+
